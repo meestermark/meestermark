e3f5b13f98c2010cdb2538151849108cf812c3ff
Marcus Brandes <MarcusBrandes@gmail.com>
2021-05-16 11:51:23 +0200

    trying an old website

--- README.md
+++
@@ -1,3 +0,0 @@
-# een onderwijswebsite
-
-deze website laat je [flitsen met bloemen](bloemenflitsen/index.html).
--- main.js
+++ main.js
@@ -1,39 +1,13 @@
-// <img src="img_orange_flowers.jpg" alt="Flowers" style="width:auto;">
+let projectsSummary = Array.from(document.getElementsByTagName('summary'));
 
-let getChosenFlowers = function () {
-	let chosenFlowers = [];
-	for (let element of document.getElementsByClassName('flowerCheckbox')) {
-		console.log(element, element.checked, element.name);
-		if (element.checked) {
-			chosenFlowers.push(element.name);
-		}
-	}
-	console.log(chosenFlowers);
-	return chosenFlowers;
-};
-
-showFlower = function (flower) {
-	return new Promise((resolve, reject) => {
-		setTimeout(() => {
-			afbeeldingSectie.innerHTML = '';
-			let picture = document.createElement('img');
-			picture.setAttribute('src', `./resources/photos_big/${flower}1.jpg`);
-			afbeeldingSectie.appendChild(picture);
-			resolve(`${flower} shown`);
-		}, 1000);
-	});
-};
-
-let flitsFlowers = async function (chosenFlowers) {
-	console.log('flitsing flowers', chosenFlowers);
-	for (let flower of chosenFlowers) {
-		console.log('next flower is ', flower);
-		let newVal = await showFlower(flower);
-		console.log(newVal);
+let toggleProject = function (e) {
+	console.log(e.target.style.cursor);
+	if (e.target.style.cursor === 'zoom-in' || e.target.style.cursor === '') {
+		e.target.style.cursor = 'zoom-out';
+	} else {
+		e.target.style.cursor = 'zoom-in';
 	}
 };
-let startFlitsen = function () {
-	console.log('huh?');
-	let chosenFlowers = getChosenFlowers();
-	flitsFlowers(chosenFlowers);
-};
+projectsSummary.forEach((projectSummary) => {
+	projectSummary.onclick = toggleProject;
+});
